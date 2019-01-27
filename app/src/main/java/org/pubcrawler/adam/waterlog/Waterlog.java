222832038d3bfda201b57d17f13825c245cbd571
adamlopresto <adamlopresto@gmail.com>
2019-01-27 10:10:29 -0600

    Gradle version

--- app/src/main/java/org/pubcrawler/adam/waterlog/Waterlog.java
+++ app/src/main/java/org/pubcrawler/adam/waterlog/Waterlog.java
@@ -164,18 +164,16 @@ public class Waterlog extends ActionBarActivity implements OnClickListener, OnLo
             return null;
         }
 
-        /*
         Calendar cal = Calendar.getInstance();
         switch (cal.get(Calendar.DAY_OF_WEEK)) {
             case Calendar.SATURDAY:
             case Calendar.SUNDAY:
-          */
                 if (drinksToday < 1)
                     return DrinkType.Coffee;
                 return DrinkType.Home;
-        /*    default:
+            default:
                 return DrinkType.Work;
-        }*/
+        }
     }
 
     @Override
